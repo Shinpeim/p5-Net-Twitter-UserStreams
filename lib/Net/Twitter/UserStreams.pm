42f230d98e31580fb633fa4b703a3720fa5066d7
Shinpei Maruyama <shinpeim@gmail.com>
2011-06-27 18:47:23 +0900

    initial commit

---
+++ lib/Net/Twitter/UserStreams.pm
@@ -0,0 +1,32 @@
+package Net::Twitter::UserStreams;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Net::Twitter::UserStreams -
+
+=head1 SYNOPSIS
+
+  use Net::Twitter::UserStreams;
+
+=head1 DESCRIPTION
+
+Net::Twitter::UserStreams is
+
+=head1 AUTHOR
+
+Shinpei Maruyama E<lt>shinpeim@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
